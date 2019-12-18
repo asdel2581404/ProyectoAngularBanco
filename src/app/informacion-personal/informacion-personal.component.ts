894abe6097ba83b1d4a0f95c4cf16feb696e2480
Evin Mosquera <evin.mosquera026@outlook.es>
2019-12-17 20:07:43 -0500

    ortografia

--- src/app/informacion-personal/informacion-personal.component.ts
+++ src/app/informacion-personal/informacion-personal.component.ts
@@ -3,16 +3,16 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { ClientesService } from '../clientes.service';
 import { MatDialog } from '@angular/material';
 import { ValidarCedulaControlComponent } from '../validar-cedula-control/validar-cedula-control.component';
-import {Persona} from '../modelos/persona';
-import {Residencia} from '../modelos/residencia';
+import { Persona } from '../modelos/persona';
+import { Residencia } from '../modelos/residencia';
 @Component({
   selector: 'app-informacion-personal',
   templateUrl: './informacion-personal.component.html',
   styleUrls: ['./informacion-personal.component.css']
 })
 export class InformacionPersonalComponent implements OnInit {
-  public  modeloInformacionPersonal=new Persona();
-  public modeloInformacionResidencia=new Residencia();
+  public modeloInformacionPersonal = new Persona();
+  public modeloInformacionResidencia = new Residencia();
   public formGroup: FormGroup;
   public formGroup2: FormGroup;
 
@@ -27,8 +27,8 @@ export class InformacionPersonalComponent implements OnInit {
   public Departamento: any;
   public Ciudad: any;
   public ValidarDelitos: any;
-  
- 
+
+
   ngOnInit() {
     this.buildForm();
     this.llenarEstadoCivil();
@@ -61,7 +61,7 @@ export class InformacionPersonalComponent implements OnInit {
     })
   }
   public LlenarPais() {
-    
+
     this.clientesService.getPais().subscribe(response => {
       this.Pais = response;
     })
@@ -70,23 +70,23 @@ export class InformacionPersonalComponent implements OnInit {
   }
   public LlenarDepartamento() {
 
-    if (this.formGroup2.get('pais').value != null && this.formGroup2.get('pais').value !="") { 
-    this.clientesService.getDepartamentos(this.formGroup2.get('pais').value).subscribe(response => {
-      this.Departamento = response;
-      this.LlenarCiudad();
-    })
-    
-  }
-    
+    if (this.formGroup2.get('pais').value != null && this.formGroup2.get('pais').value != "") {
+      this.clientesService.getDepartamentos(this.formGroup2.get('pais').value).subscribe(response => {
+        this.Departamento = response;
+        this.LlenarCiudad();
+      })
+
+    }
+
   }
 
   public LlenarCiudad() {
 
-    if(this.formGroup2.get('departamento').value!=null && this.formGroup2.get('departamento').value!=""){
-    this.clientesService.getCiudad(this.formGroup2.get('departamento').value).subscribe(response => {
-      this.Ciudad = response;
-    })
-  }
+    if (this.formGroup2.get('departamento').value != null && this.formGroup2.get('departamento').value != "") {
+      this.clientesService.getCiudad(this.formGroup2.get('departamento').value).subscribe(response => {
+        this.Ciudad = response;
+      })
+    }
 
   }
 
@@ -116,40 +116,40 @@ export class InformacionPersonalComponent implements OnInit {
   submit() {
 
 
-    if (this.formGroup2.valid && this.formGroup.valid && this.ValidarDelitos ==false) {
+    if (this.formGroup2.valid && this.formGroup.valid && this.ValidarDelitos == false) {
       this.notify.emit(this.formGroup);
       this.ModeloInformacionPersonal.emit(this.formGroup.value)
-      this.modeloInformacionResidencia=this.formGroup2.value;
-      this.modeloInformacionResidencia.idCliente=this.formGroup.get('cedula').value;
+      this.modeloInformacionResidencia = this.formGroup2.value;
+      this.modeloInformacionResidencia.idCliente = this.formGroup.get('cedula').value;
 
       this.ModeloInformacionResidencia.emit(this.modeloInformacionResidencia)
-     
+
     }
-    else{
-      
+    else {
+
     }
-   
+
   }
 
   ValidarCedula() {
     console.log(this.formGroup.get('cedula').value)
-      if(this.formGroup.get('cedula').value!=null && this.formGroup.get('cedula').value!="" ){
-    this.clientesService.getValidarCedula(this.formGroup.get('cedula').value).subscribe(response => {
-      this.ValidarDelitos = response;
+    if (this.formGroup.get('cedula').value != null && this.formGroup.get('cedula').value != "") {
+      this.clientesService.getValidarCedula(this.formGroup.get('cedula').value).subscribe(response => {
+        this.ValidarDelitos = response;
 
-      if (this.ValidarDelitos != false) {
+        if (this.ValidarDelitos != false) {
 
-        let dialogRef = this.dialog.open(ValidarCedulaControlComponent, {
-          data: 'Señor usuario hemos encontrado una inhabilidad para poder continuar el proceso, para mas informacion comuniquese al 0180098989',
-          width: '30%',
-          height: '30%'
-        });
-        dialogRef.afterClosed().subscribe(response => {
-          console.log(response);
-        })
-      }
-    })
-  }
+          let dialogRef = this.dialog.open(ValidarCedulaControlComponent, {
+            data: 'Señor usuario hemos encontrado una inhabilidad para poder continuar el proceso, para mas informacion comuniquese al 0180098989',
+            width: '30%',
+            height: '30%'
+          });
+          dialogRef.afterClosed().subscribe(response => {
+            
+          })
+        }
+      })
+    }
   }
 
 }
